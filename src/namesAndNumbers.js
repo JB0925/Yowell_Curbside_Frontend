6c7605829bfc95ea023cfeb3f92a4243b0830aa0
Jesse Brink <jbrink0925@gmail.com>
2022-02-08 22:15:31 -0500

    add files and create dummy list of names for testing

---
+++ src/namesAndNumbers.js
@@ -0,0 +1,14 @@
+let studentList = {
+  1: "Kyle Smith",
+  2: "Joe Thomas",
+  3: "Sally Harris",
+  4: "John Edwards",
+  5: "Emily Newman",
+  6: "Sarah Rodriguez",
+  7: "Tristan Thompson",
+  8: "Carlos Hernandez",
+  9: "Alison White",
+  10: "Jeremiah Lewis",
+};
+
+export default studentList;
