e9acd62ea9f356116f0bf90781328128e73b47b1
Jesse Brink <jbrink0925@gmail.com>
2022-04-23 20:33:02 -0400

    added way for curbside staff to see what next number to add should be.

--- src/mainList.js
+++ src/mainList.js
@@ -27,13 +27,17 @@ export default function MainList() {
     ));
   };
 
+  const getNextNumberToAdd = () => {
+    if (!everyStudent.length) return;
+    const numbersLessThanFourHundred = everyStudent.map(
+      (student) => parseInt(student.number) < 400
+    );
+    return numbersLessThanFourHundred[numbersLessThanFourHundred.length - 1];
+  };
+
   return (
     <div className="allStudentsContainer">
-      <p className="nextNumber">{`Next number should be: ${
-        everyStudent.length
-          ? parseInt(everyStudent[everyStudent.length - 1].number) + 1
-          : null
-      }`}</p>
+      <p className="nextNumber">{`Next number should be: ${getNextNumberToAdd()}`}</p>
       {displayAllStudents()}
     </div>
   );
