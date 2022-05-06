d24d488b70b2b9beb8ad4fe621af976e28b9c719
Jesse Brink <jbrink0925@gmail.com>
2022-05-06 14:58:29 -0400

    change background color of html element on toggle.

--- src/toggleSwitch.js
+++ src/toggleSwitch.js
@@ -5,12 +5,15 @@ export default function ToggleSwitch({ checkState }) {
   const { isChecked, setIsChecked } = checkState;
   const changeBackground = () => {
     if (!isChecked) {
+      document.documentElement.style = "white";
       document.body.style.backgroundColor = "white";
       document.body.style.color = "#0c162e";
     } else {
+      document.documentElement.style = "#0c162e";
       document.body.style.backgroundColor = "#0c162e";
       document.body.style.color = "white";
     }
+    console.log(document.documentElement);
     setIsChecked(!isChecked);
   };
 
