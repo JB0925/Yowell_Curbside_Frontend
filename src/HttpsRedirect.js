8ed93d62737b1f26a1504806604e746e714d620a
Jesse Brink <jbrink0925@gmail.com>
2022-07-27 23:31:27 -0400

    add static.json file for https redirection

---
+++ src/HttpsRedirect.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react";
+
+const UseRedirectToHttps = () => {
+  let pathname = window.location.pathname;
+  useEffect(() => {
+    if (
+      window.location.protocol !== "https:" &&
+      process.env.NODE_ENV === "production"
+    ) {
+      window.location.replace(
+        "https://nameless-wave-46063.herokuapp.com" + pathname
+      );
+    }
+  }, []);
+};
+
+export default UseRedirectToHttps;
